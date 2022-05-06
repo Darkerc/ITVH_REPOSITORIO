d0c121e257cfbf516f2a20c56965a04076323c5c
Darkerc <ederastonmartin@hotmail.com>
2022-05-06 14:04:34 -0500

    Empezando index

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,87 +17,92 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>" class="h-100">
+
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
+
 <body class="d-flex flex-column h-100">
-<?php $this->beginBody() ?>
+    <?php $this->beginBody() ?>
 
-<header>
-    <?php
-    NavBar::begin([
-        'brandLabel' => 'ITVH Repositorio',
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
-        ],
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
-        'items' => [
-            ['label' => 'Inicio', 'url' => Yii::$app->homeUrl],
-            /*['label' => 'Inicio', 'url' => ['/autor/index']],*/
-            [
-                'label' => 'Listado',
-                'items' => [
-                     ['label' => 'Autores', 'url' => ['/autor/index']],
-                     ['label' => 'Archivos', 'url' => ['/archivo/index']],
-                ],
+    <header>
+        <?php
+        NavBar::begin([
+            'brandLabel' => 'ITVH Repositorio',
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
             ],
-            [
-                'label' => 'Frontend routes',
-                'items' => [
-                    ['label'=>'Login', 'url'=>['/user-management/auth/login']],
-                    ['label'=>'Logout', 'url'=>['/user-management/auth/logout']],
-                    ['label'=>'Registration', 'url'=>['/user-management/auth/registration']],
-                    ['label'=>'Change own password', 'url'=>['/user-management/auth/change-own-password']],
-                    ['label'=>'Password recovery', 'url'=>['/user-management/auth/password-recovery']],
-                    ['label'=>'E-mail confirmation', 'url'=>['/user-management/auth/confirm-email']],
+        ]);
+        echo Nav::widget([
+            'options' => ['class' => 'navbar-nav'],
+            'items' => [
+                ['label' => 'Inicio', 'url' => Yii::$app->homeUrl],
+                /*['label' => 'Inicio', 'url' => ['/autor/index']],*/
+                [
+                    'label' => 'Listado',
+                    'items' => [
+                        ['label' => 'Autores', 'url' => ['/autor/index']],
+                        ['label' => 'Archivos', 'url' => ['/archivo/index']],
+                    ],
                 ],
-            ],
-            ['label' => 'Backend routes',
-            'items'=>UserManagementModule::menuItems()],
-            Yii::$app->user->isGuest ? (
-                //['label' => 'Login', 'url' => ['/site/login']]
-                ['label' => 'Login', 'url' => ['/user-management/auth/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/user-management/auth/logout'], 'post', ['class' => 'form-inline'])
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
+                [
+                    'label' => 'Frontend routes',
+                    'items' => [
+                        ['label' => 'Login', 'url' => ['/user-management/auth/login']],
+                        ['label' => 'Logout', 'url' => ['/user-management/auth/logout']],
+                        ['label' => 'Registration', 'url' => ['/user-management/auth/registration']],
+                        ['label' => 'Change own password', 'url' => ['/user-management/auth/change-own-password']],
+                        ['label' => 'Password recovery', 'url' => ['/user-management/auth/password-recovery']],
+                        ['label' => 'E-mail confirmation', 'url' => ['/user-management/auth/confirm-email']],
+                    ],
+                ],
+                [
+                    'label' => 'Backend routes',
+                    'items' => UserManagementModule::menuItems()
+                ],
+                Yii::$app->user->isGuest ? (
+                    //['label' => 'Login', 'url' => ['/site/login']]
+                    ['label' => 'Login', 'url' => ['/user-management/auth/login']]
+                ) : ('<li>'
+                    . Html::beginForm(['/user-management/auth/logout'], 'post', ['class' => 'form-inline'])
+                    . Html::submitButton(
+                        'Logout (' . Yii::$app->user->identity->username . ')',
+                        ['class' => 'btn btn-link logout']
+                    )
+                    . Html::endForm()
+                    . '</li>'
                 )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
-    ]);
-    NavBar::end();
-    ?>
-</header>
+            ],
+        ]);
+        NavBar::end();
+        ?>
+    </header>
 
-<main role="main" class="flex-shrink-0">
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</main>
+    <main role="main" class="flex-shrink-0">
+        <div class="container">
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <?= Alert::widget() ?>
+            <?= $content ?>
+        </div>
+    </main>
 
-<!-- <footer class="footer mt-auto py-3 text-muted">
+    <!-- <footer class="footer mt-auto py-3 text-muted">
     <div class="container">
         <p class="float-left">&copy; My Company <?= date('Y') ?></p>
         <p class="float-right"><?= Yii::powered() ?></p>
     </div>
 </footer> -->
 
-<?php $this->endBody() ?>
+    <?php $this->endBody() ?>
 </body>
+
 </html>
 <?php $this->endPage() ?>
--- views/site/index.php
+++ views/site/index.php
@@ -6,47 +6,86 @@ $this->title = 'ITVH Repositorio';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Bienvenidos!</h1>
-
-        <p class="lead">Este es el Repositorio Institucional del ITVH.</p>
-
+    <div class="jumbotron text-center bg-transparent brand">
+        <h4>Repositorio institucional del ITVH</h4>
     </div>
 
     <div class="body-content">
-
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            <div class="col-12 col-lg-8">
+                <div class="card bg-light">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Repositorios recientes</h5>
+                        <div id="carouselExampleControls" class="carousel slide carousel-dark" data-ride="carousel" style="height: 200px;">
+                            <ol class="carousel-indicators">
+                                <li style="background-color: #888;" data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
+                                <li style="background-color: #888;" data-target="#carouselExampleControls" data-slide-to="1"></li>
+                                <li style="background-color: #888;" data-target="#carouselExampleControls" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <div class="d-flex flex-column justify-content-center">
+                                        <h5 class="my-1 text-center">
+                                            Índice de Cohesión Organizacional: Propuesta para Evaluar la Guía Corporativa
+                                        </h5>
+                                        <div class="text-center my-2">
+                                            De: Roberto Celaya
+                                        </div>
+                                        <button type="button" class="btn btn-info btn-sm d-inline mx-auto my-2">Ver repositorio</button>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="d-flex flex-column justify-content-center">
+                                        <h5 class="my-1 text-center">
+                                            Índice de Cohesión Organizacional: Propuesta para Evaluar la Guía Corporativa
+                                        </h5>
+                                        <div class="text-center my-2">
+                                            De: Roberto Celaya
+                                        </div>
+                                        <button type="button" class="btn btn-info btn-sm d-inline mx-auto my-2">Ver repositorio</button>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <div class="d-flex flex-column justify-content-center">
+                                        <h5 class="my-1 text-center">
+                                            Índice de Cohesión Organizacional: Propuesta para Evaluar la Guía Corporativa
+                                        </h5>
+                                        <div class="text-center my-2">
+                                            De: Roberto Celaya
+                                        </div>
+                                        <button type="button" class="btn btn-info btn-sm d-inline mx-auto my-2">Ver repositorio</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                <i class="material-icons" style="color: #000 !important;">arrow_back_ios</i>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                <i class="material-icons" style="color: #000 !important;">arrow_forward_ios</i>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            <div class="col-12 col-lg-4">
+                <div class="row">
+                    <div class="col-12">
+                        Contenido2
+                    </div>
+                </div>
             </div>
         </div>
-
+        <div class="row">
+            <div class="col-12 col-md-8">
+                <div class="row">
+                    <div class="col-12">
+                        Contenido2
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4">Contenido2</div>
+        </div>
     </div>
 </div>

bb1a3ddd2e58f2f6c411e0ac3be91fc6373de889
Darkerc <ederastonmartin@hotmail.com>
2022-05-20 09:57:10 -0500

    Añadiendo cambios a publicacion

---
+++ views/site/archivo.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'ITVH Repositorio';
+?>
+<div class="site-index">
+    <div class="py-3 text-center bg-transparent brand">
+        <h4>Archivo</h4>
+    </div>
+
+    <div class="body-content">
+
+    </div>
+</div>

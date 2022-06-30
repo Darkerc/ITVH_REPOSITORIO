2e350f183ecfd640c1d72c82b0294e09971a8928
xXDarkNobleXx <leonardo940@live.com.mx>
2022-06-30 15:12:18 -0500

    Agregando el modal del gridview

--- views/recurso/_card.php
+++ views/recurso/_card.php
@@ -92,6 +92,14 @@ $archivos = new ArrayDataProvider([
                         </td>
                     </tr>
                     <tr class="tr_item">
+                        <td class="td_header">URL del recurso</td>
+                        <td class="td_value">
+                            <a href="<?= $imagick ?>">
+                                <?= $model->currentUrl ?>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr class="tr_item">
                         <td class="td_header">Palabras Clave</td>
                         <td class="td_value">
                             <?php
@@ -130,22 +138,25 @@ $archivos = new ArrayDataProvider([
                     <?php } ?>
                 </table>
             </div>
-
-            <?php Modal::begin([
-                'title' => '<h2>Hello world</h2>',
-                'toggleButton' => ['label' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
-                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-                <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-              </svg>'],
-                'bodyOptions' => ['class' => 'kv-file-download btn btn-sm btn-kv btn-default btn-outline-secondary']
-            ]);
-
-            echo 'Say hello...';
-
-            Modal::end();
-            ?>
-
             <div style="width: 100%;">
+                <?php
+                $this->registerJs("$(function() {
+                    $('#popupModal').click(function(e) {
+                      e.preventDefault();
+                      $('#modal').modal('show').find('.modal-content')
+                      .load($(this).attr('href'));
+                    });
+                 });");
+
+                Modal::begin([
+                    'id' => 'modal',
+                    'title' => '<h2>' . $model->rec_nombre . '</h2>',
+                ]);
+                echo '<pre>';
+                var_dump($model->rec_resumen);
+                echo '<pre>';
+                Modal::end();
+                ?>
                 <?=
                 GridView::widget([
                     'dataProvider' => $archivos,
@@ -159,10 +170,10 @@ $archivos = new ArrayDataProvider([
                             'header' => ' ',
                             'template' => '{myButtonview}{myButton}',
                             'buttons' => [
-                                'myButton' => function ($url, $archivo, $key) {     // render your custom button
-                                    return Html::a('<img src="/images/view.svg" />', $archivo->getArchivoURL(), ['class' => 'kv-file-download btn btn-sm btn-kv btn-default btn-outline-secondary', 'title' => 'Ver']);
-                                },
                                 'myButtonview' => function ($url, $archivo, $key) {     // render your custom button
+                                    return Html::a('<img src="/images/view.svg" />', ['href' => ''], ['class' => 'kv-file-download btn btn-sm btn-kv btn-default btn-outline-secondary', 'title' => 'Ver', 'id' => 'popupModal']);
+                                },
+                                'myButton' => function ($url, $archivo, $key) {     // render your custom button
                                     return Html::a('<img src="/images/download.svg" />', $archivo->getArchivoURL(), ['class' => 'kv-file-download btn btn-sm btn-kv btn-default btn-outline-secondary', 'title' => 'Descargar']);
                                 }
                             ]
